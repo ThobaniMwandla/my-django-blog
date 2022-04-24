6158bbe51e1d09986ba6484e574c99f6d3422c2b
ThobaniMwandla <gmwandla@gmail.com>
2022-04-24 16:26:10 +0200

    fisrt function view

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def article_list(request):
+    return HttpResponse("This is our first view")
